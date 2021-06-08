215bb8a7b750f62b77d9d973917bf04f9c5c24cc
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-08 13:46:27 -0400

    DLL push and pop methods working

--- DoublyLinkedList/classAndMethods.js
+++ DoublyLinkedList/classAndMethods.js
@@ -1,3 +1,5 @@
+const print = console.log
+
 class Node{
   constructor(val){
     this.val = val
@@ -13,10 +15,49 @@ class DoublyLinkedList{
     this.length = 0
   }
 
+  push(val){
+    let newNode = new Node(val)
+    if(!this.length) {
+      this.head = newNode
+      this.tail = this.head
+    }
+    else{
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
 
-
-
+  pop(){
+    if(this.length === 0) return undefined
+    if(this.length === 1) {
+      this.length--
+      this.head = null
+      this.tail = this.head
+      return this
+    }
+    let prev = this.tail.prev
+    this.tail.prev = null
+    this.tail = prev
+    prev.next = null
+    this.length--
+    return this
+  }
 
 
 
 }
+
+let list = new DoublyLinkedList()
+list.push(5)
+list.push(15)
+list.push(6)
+list.push(9)
+list.push(100)
+
+print(list)
+
+print(list.pop())
+print(list)

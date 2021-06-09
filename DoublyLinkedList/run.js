03c8bdba706cdfc7bab3ec0d29ab14cea0d92eef
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-08 21:48:23 -0400

    stack implementation with push and pop methods

--- DoublyLinkedList/run.js
+++ DoublyLinkedList/run.js
@@ -1,12 +1,4 @@
 const {nodeClass, dllClass}= require('./classAndMethods')
 const print = console.log
 
-let list = new dllClass()
-list.push(5)
-list.push(15)
-list.push(6)
-list.push(9)
-list.push(100)
-print(list)
-print(list.remove(2))
-print(list.get(2))
+

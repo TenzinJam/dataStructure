6786520f2d22d915ea2f7d12c5927a6afa339b27
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-09 00:20:43 -0400

    BST implementation with insertion and finding method

---
+++ trees/tree.js
@@ -0,0 +1,56 @@
+class Node{
+  constructor(val){
+    this.val = val
+    this.right = null
+    this.left = null
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.root = null
+  }
+
+  insert(val){
+    let newNode = new Node(val)
+    if(!this.root) {
+      this.root = newNode
+      return this
+    }
+    let current = this.root
+    while(current !== null){
+      if(val === current.val) return `${val} already exists in the tree`
+      let leaf = current
+      if(val < current.val) {
+        current = current.left
+        if(!current) leaf.left = newNode
+      }
+      else {
+        current = current.right
+        if(!current) leaf.right = newNode
+      }
+    }
+    return this
+  }
+
+  find(val){
+    if(!this.root === null) return null
+    let current = this.root
+    while(current !== null){
+      if(val === current.val) return true
+      if(val < current.val) current = current.left
+      else current = current.right
+    }
+    return false
+  }
+}
+
+let BST = new BinarySearchTree()
+
+BST.insert(9)
+BST.insert(10)
+BST.insert(20)
+BST.insert(4)
+BST.insert(5)
+console.log(BST)
+console.log(BST.find())

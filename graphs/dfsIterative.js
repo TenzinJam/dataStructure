49dcef0f62a4b29df5d3c9bdbc066e2abc253cff
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-23 13:07:41 -0400

    graph dfs iterative approach is working as well. the order will be a bit different from the recursive one because in this case we are popping from the stack

--- graphs/dfsIterative.js
+++ graphs/dfsIterative.js
@@ -1,11 +1,24 @@
-let graph = {something}
+const graph = require('./graph')
+let print = console.log
 
-function dfsIterative(start){
+graph.dfsIterative = function(start){
   let stack = [start]
   let result = []
-  let visited = {start: true}
+  let visited = {}
+  let current
+  visited[start] = true
   while(stack.length){
-    
+    console.log(stack)
+    current = stack.pop()
+    result.push(current)
+    this.adjacencyList[current].forEach(neighbor => {
+      if(!visited[neighbor]){
+        visited[neighbor] = true
+        stack.push(neighbor)
+      }
+    })
   }
-
+  return result
 }
+
+print(graph.dfsIterative("A"))

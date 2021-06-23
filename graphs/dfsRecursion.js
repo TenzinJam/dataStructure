e4ba0e1fd849e056bdca9a7c0efe34c75f01c0a6
Tenzin Jamyang <jamyangu@gmail.com>
2021-06-23 11:42:46 -0400

    done with recursive approach. Need to compare the answers and make the changes

--- graphs/dfsRecursion.js
+++ graphs/dfsRecursion.js
@@ -1,3 +1,6 @@
+
+let graph = [something]
+
 function dfsRecursive(vertex){
   if(vertex===null) return
   let result = []
@@ -5,6 +8,13 @@ function dfsRecursive(vertex){
   function helper(vertex){
     if(vertex===null) return
     visited.push(vertex)
-    
+    result.push(vertex)
+    for(let node in graph.adjacencyList(vertex)){
+      if(!visited[node]){
+        helper(node)
+      }
+    }
   }
+  helper(vertex)
+  return result
 }
